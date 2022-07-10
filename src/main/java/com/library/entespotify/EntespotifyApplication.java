a751bbe602eaba0337727cd3333c7664da16f50d
Ankith Sajikumar <ankith.sajikumar@ihg.com>
2022-07-10 20:55:34 +0530

    Initial commit

---
+++ src/main/java/com/library/entespotify/EntespotifyApplication.java
@@ -0,0 +1,13 @@
+package com.library.entespotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntespotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntespotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library/entespotify/EntespotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.entespotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntespotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
