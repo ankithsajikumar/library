7d5ebbc359b580f8bfa0196afdc8efc9b28456da
Ankith Sajikumar <ankith.sajikumar@ihg.com>
2022-07-13 16:04:44 +0530

    trying db connect

--- src/main/java/com/library/entespotify/EntespotifyApplication.java
+++ src/main/java/com/library/entespotify/EntespotifyApplication.java
@@ -2,8 +2,10 @@ package com.library.entespotify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(scanBasePackages = "com.library.entespotify")
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class EntespotifyApplication {
 
 	public static void main(String[] args) {
