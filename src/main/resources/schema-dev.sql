21d6cec61aebc5e606ca6929ec80ad64c5c91dc3
Ankith Sajikumar <ankith.sajikumar@ihg.com>
2022-09-24 18:48:27 +0530

    sql schema testing

---
+++ src/main/resources/schema-dev.sql
@@ -0,0 +1,45 @@
+CREATE TABLE users (
+    id   INTEGER      NOT NULL,
+    userName VARCHAR(128),
+    password VARCHAR(128),
+    active BOOLEAN,
+    roles VARCHAR(128),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tracks (
+    id   INTEGER      NOT NULL,
+    title VARCHAR(128),
+    album VARCHAR(128),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE refreshtoken (
+    id   INTEGER      NOT NULL,
+    user_id INTEGER,
+    token VARCHAR(128),
+    expiry_date DATETIME,
+    PRIMARY KEY (id)
+);
+CREATE TABLE artists (
+    id   INTEGER      NOT NULL,
+    name VARCHAR(128),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE artist_albums (
+    artist_id   INTEGER,
+    album_id   INTEGER,
+    PRIMARY KEY (artist_id)
+);
+
+CREATE TABLE artist_tracks (
+    artist_id   INTEGER,
+    track_id   INTEGER,
+    PRIMARY KEY (artist_id)
+);
+CREATE TABLE albums (
+    id   INTEGER      NOT NULL,
+    title VARCHAR(128),
+    PRIMARY KEY (id)
+);
